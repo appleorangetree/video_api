84288a4de6cbeeafdcc1465437d2bb4f53785553
appleorangetree <308600986@qq.com>
2021-05-02 23:44:23 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello world");
+
+let msg =" hello every one"
+
+console.log(msg);
+
+let a = 3;
+let b = 2;
+c=a*b
+
+console.log(c);
+
+let fruit =["banana","pomme"];
+console.log(fruit);
